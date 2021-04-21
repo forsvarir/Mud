6642645973057215df2d05390af216572b02c1c1
Mark Loveland-Armour <forsvarir@blueyonder.co.uk>
2021-04-21 15:37:03 +0100

    Automatically Case player names.  Allow session search on mis-cased names

--- src/main/java/com/forsvarir/mud/SessionManager.java
+++ src/main/java/com/forsvarir/mud/SessionManager.java
@@ -2,6 +2,7 @@ package com.forsvarir.mud;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Locale;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Service
@@ -9,9 +10,14 @@ public class SessionManager {
     private final ConcurrentHashMap<String, Player> playersByPrincipalSession = new ConcurrentHashMap<>();
 
     public Player createSession(String principalName, String sessionId, String playerName) {
-        final Player newPlayer = new Player(playerName, principalName, sessionId);
+        final Player newPlayer = new Player(capitalisePlayerName(playerName), principalName, sessionId);
         playersByPrincipalSession.put(buildSessionKey(principalName, sessionId), newPlayer);
         return newPlayer;
+    }
+
+    private String capitalisePlayerName(String playerName) {
+        return playerName.substring(0, 1).toUpperCase(Locale.ROOT)
+                + playerName.substring(1).toLowerCase(Locale.ROOT);
     }
 
     public Player findPlayer(String principalName, String sessionId) {
@@ -19,8 +25,9 @@ public class SessionManager {
     }
 
     public Player findPlayer(String playerName) {
+        var targetPlayer = capitalisePlayerName(playerName);
         return playersByPrincipalSession.search(0, (key, player) -> {
-            if (player.getName().equals(playerName)) return player;
+            if (player.getName().equals(targetPlayer)) return player;
             return null;
         });
     }
--- src/test/java/com/forsvarir/mud/SessionManagerTest.java
+++ src/test/java/com/forsvarir/mud/SessionManagerTest.java
@@ -10,28 +10,60 @@ class SessionManagerTest {
 
     @Test
     void createSession_returnsCreatedPlayer() {
-        var player = sessionManager.createSession("principal1", "session1", "player1");
+        var player = sessionManager.createSession("principal1", "session1", "Player1");
 
-        assertThat(player.getName()).isEqualTo("player1");
+        assertThat(player.getName()).isEqualTo("Player1");
         assertThat(player.getSessionId()).isEqualTo("session1");
         assertThat(player.getPrincipal()).isEqualTo("principal1");
     }
 
     @Test
+    void createSession_capitalisesPlayerName() {
+        var player = sessionManager.createSession("principal1", "session1", "player1");
+
+        assertThat(player.getName()).isEqualTo("Player1");
+    }
+
+    @Test
+    void createSession_lowerCasesTailOfPlayerName() {
+        var player = sessionManager.createSession("principal1", "session1", "PLAYER1");
+
+        assertThat(player.getName()).isEqualTo("Player1");
+    }
+
+    @Test
     void findPlayer_byPrincipalAndSession_returnsPlayer() {
-        sessionManager.createSession("Principal", "sessionId", "PlayerName");
+        sessionManager.createSession("Principal", "sessionId", "Player2");
 
         var player = sessionManager.findPlayer("Principal", "sessionId");
 
-        assertThat(player.getName()).isEqualTo("PlayerName");
+        assertThat(player.getName()).isEqualTo("Player2");
     }
 
     @Test
     void findPlayer_byPlayerName_returnsPlayer() {
-        sessionManager.createSession("Principal", "sessionId", "PlayerName");
+        sessionManager.createSession("Principal", "sessionId", "Player");
 
-        var player = sessionManager.findPlayer("PlayerName");
+        var player = sessionManager.findPlayer("Player");
 
-        assertThat(player.getName()).isEqualTo("PlayerName");
+        assertThat(player.getName()).isEqualTo("Player");
+    }
+
+    @Test
+    void findPlayer_byCaseMismatchedPlayerName_returnsPlayer() {
+        sessionManager.createSession("Principal", "sessionId", "Player");
+
+        var player = sessionManager.findPlayer("player");
+
+        assertThat(player.getName()).isEqualTo("Player");
+    }
+
+    @Test
+    void findPlayer_singleCharacterName_returnsPlayer() {
+        sessionManager.createSession("Principal", "sessionId", "P");
+
+        var player = sessionManager.findPlayer("P");
+
+        assertThat(player.getName()).isEqualTo("P");
     }
 }
